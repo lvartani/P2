2f825b8d329147388eefd2c511bcb357cdc5af03
Loosine Vartani <lvartani.gmail.com>
2016-02-18 01:00:47 -0500

    Add files

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>P2</title>
+    <meta charset = 'utf-8'>
+
+    <link rel='stylesheet' href='theme.css' type ='text/css'>
+
+
+</head>
+<body>
+    <div id = generator>
+        <h3>PASSWORD GENERATOR</h3>
+        <form method='GET' action='display.php'>
+            Choose Characters<br>
+            <input maxlength=1 type='text' name='number_words' value ='4'><br>
+            <input type='checkbox' name='add_number' id='add_number' unchecked>
+            <label for='add_number'>Add a number</label><br>
+            <input type='checkbox' name='add_symbol' id='add_symbol' unchecked>
+            <label for='add_symbol'>Add a symbol</label><br>
+            <input type='submit' class='btn btn-info'value='Generate!'><br>
+        </form>
+    </div>
+
+    <p class='password'>  <?php require('logic.php'); ?> </p>
+    <a href='http://xkcd.com/936/'>
+        <img class='comic' src='http://imgs.xkcd.com/comics/password_strength.png' alt='xkcd style passwords'>
+    </a>
+    <!-- Google Font -->
+    <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css' />
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    </html>
---
+++ logic.php
@@ -0,0 +1,49 @@
+<?php
+//Bring in words from paulnoll.com
+$doc = new DOMDocument();
+$doc->loadHTML(file_get_contents("words.html"));
+$list = $doc->getElementsByTagName('li');
+//Create empty word content array
+$words_array =[];
+
+//Loop through to get to string values rather than DOM objects, then push to array
+foreach ($list as $word) {
+    $newlist = "{$word->nodeValue}";
+    array_push($words_array, $newlist);
+}
+//Create arrays for symbols and numbers
+$symbols = array("!","@","#","$","%","^","&","*","-","_","+","=");
+
+$numbers = array("0","1","2","3","4","5","6","7","8","9",);
+
+//Set variables for form values and check if it is not null
+$input_words = (isset($_GET['number_words']) ? $_GET['number_words'] : null);;
+$input_num = (isset($_GET['add_number']) ? $_GET['add_number'] : null);
+$input_sym = (isset($_GET['add_symbol']) ? $_GET['add_symbol'] : null);
+
+//Set default number of words to 4 for server-side validation
+if ($input_words < 2 || is_numeric($input_words)==false){
+    $input_words = 4;
+}
+//Create array with random words, numbers, and symbols
+$rand_keys = array_rand($words_array, $input_words);
+for ($i = 0; $i < $input_words; $i++){
+    echo $words_array[$rand_keys[$i]] . "-";
+}
+
+if ($input_num == "on"){
+    $rand_keys = array_rand($numbers, $input_words);
+    echo $numbers[$rand_keys[0]];
+}
+else{
+    echo "";
+}
+if ($input_sym == "on"){
+    $rand_keys = array_rand($symbols, $input_words);
+    echo $symbols[$rand_keys[0]];
+}
+else{
+    echo "";
+}
+
+?>
